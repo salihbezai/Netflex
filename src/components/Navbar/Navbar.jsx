6226a64f227ea6ed5a62e398e70aabe1a923f338
salihbezai <salihbezai98@gmail.com>
2025-02-25 13:09:50 +0100

    fixing hamburger menu issues

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom"
 import "./navbar.css"
 const Navbar = () => {
+
+  const toggleHamburger=()=>{
+    const hamburger = document.getElementById("hamburger");
+    const listItems = document.getElementsByClassName('list-items')[0]
+    hamburger.classList.toggle("active")
+    listItems.classList.toggle("active")
+  }
   return (
     <nav>
       
       <a href="/" className="logo">
        NETFLEX
       </a>
+      <div onClick={toggleHamburger} class="hamburger" id="hamburger">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
       <ul className="list-items">
         <li className="item">
         <Link to={"/"} >Home</Link>
