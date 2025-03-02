0480808f2bf35ab15cb0fa47ed76dca5dfa4cd98
salihbezai <salihbezai98@gmail.com>
2025-03-02 15:59:08 +0100

    close nav once click out the list-items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
 
   const toggleHamburger=()=>{
     setOpen(!open)
-   
     const hamburger = document.getElementById("hamburger");
     const listItems = document.getElementsByClassName('list-items')[0]
     const nav = document.getElementById('nav')
@@ -16,12 +15,31 @@ const Navbar = () => {
   }
 
   useEffect(() => {
+    const hamburger = document.getElementById("hamburger");
+    const listItems = document.getElementsByClassName('list-items')[0]
     if(open){
       document.body.classList.add("no-scroll");
+      hamburger.classList.add("active")
+    listItems.classList.add('active')
     }else{
       document.body.classList.remove("no-scroll");
+      hamburger.classList.remove("active")
+      listItems.classList.remove('active')
     }
+
   }, [open])
+
+  document.addEventListener('click',(e)=>{
+    const listItems = document.getElementsByClassName('list-items')[0]
+    const hamburger = document.getElementById("hamburger");
+    console.log("the target value "+e.target)
+    console.log("hamburger "+hamburger.contains(e.target))
+    if(!listItems.contains(e.target) && !hamburger.contains(e.target)){
+      setOpen(false)
+    }
+  })
+
+
   
 
 
