3d90b23bda19834eceea5509f01db4c4c5ba8345
salihbezai <salihbezai98@gmail.com>
2025-03-02 12:25:57 +0100

    disalbe scroll behavior on nav open

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,41 @@
 import { Link } from "react-router-dom"
 import "./navbar.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 const Navbar = () => {
   const [open, setOpen] = useState(false)
+
+
   const toggleHamburger=()=>{
     setOpen(!open)
+   
     const hamburger = document.getElementById("hamburger");
     const listItems = document.getElementsByClassName('list-items')[0]
     const nav = document.getElementById('nav')
     hamburger.classList.toggle("active")
     listItems.classList.toggle("active")
-   
   }
+
+  useEffect(() => {
+    if(open){
+      document.body.classList.add("no-scroll");
+    }else{
+      document.body.classList.remove("no-scroll");
+    }
+  }, [open])
+  
+
+
   return (
     <nav id="nav">
-            <div id="overlay" className={open ? "overlay active":"overlay"}></div>
+      <div id="overlay" className={open ? "overlay active":"overlay"}></div>
 
       <a href="/" className="logo">
        NETFLEX
       </a>
       <div onClick={toggleHamburger} class="hamburger" id="hamburger">
-      <span></span>
-      <span></span>
-      <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
     </div>
       <ul className="list-items">
         <li className="item">
